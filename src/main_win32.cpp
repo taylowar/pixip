420d075c1fc96a18700129ad7a023b831d20a96f
tilen.okretic <tilen.okretic@gmail.com>
2026-02-15 11:44:51 +0100

    update: yep I forgot something

--- src/main_win32.cpp
+++ src/main_win32.cpp
@@ -11,12 +11,9 @@ int main(void)
     SL_Settings settings = {};
     sl_read_settings_from_file("./pixip.conf", &settings);
 
-    const char* monitor_dir_path = settings.monitor_dir_path.c_str();
-    const char* trash_bin_path = settings.trash_bin_path.c_str();
-
     Dmon dmon = {};
     dmon_init(&dmon);
-    dmon_register_directory(&dmon, monitor_dir_path, dmon_notify_CREATE);
+    dmon_register_directory(&dmon, settings.monitor_dir_path, dmon_notify_CREATE);
 
     for (;;) {
         Dmon_Notify_Result dnr = {};
