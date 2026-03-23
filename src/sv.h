1aa1ca7e5098e019acd2ade90b0924bc10a740e1
tilen.okretic <tilen.okretic@gmail.com>
2026-03-23 23:48:15 +0100

    update(src/sv.h): reimplemented `sv.h`

--- src/sv.h
+++ src/sv.h
@@ -1,128 +1,204 @@
-// String View - taken from TSoding SV.h
+// StringView - sv.h - Tilen Okretic 21.02.2026
+//
+// Heavily inspired by a who? A Mr. Tsoding
+//
 
 #ifndef SV_H_
 #define SV_H_
 
-#include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-typedef struct {
-    size_t size;
-    const char* data;
-} StringView;
-
-#define SV(cstr) sv_from_cstr(cstr, sizeof(cstr)-1)
-#define SV_Fmt "%.*s"
+#define SV_FMT "%.*s"
 #define SV_ARG(sv) (int)(sv).size, (sv).data
 
-StringView sv_from_cstr(const char* cstr, size_t size);
+typedef struct {
+    const char* data;
+    size_t size;
+} StringView;
 
-bool cstr_ends_with(const char* cstr, const char* ends);
-bool sv_equals(StringView sv1, StringView sv2);
+StringView sv_from_cstr(const char* cstr);
+#define SV(cstr) sv_from_cstr((cstr))
 
-StringView sv_chop_until_delim(StringView sv, const char delim);
+// remove trailing spaces
+void sv_trim_begin(StringView *sv);
+void sv_trim_end(StringView *sv);
+void sv_trim(StringView *sv);
 
-StringView sv_extend(StringView sv, const char* cstr);
+void sv_chop_begin(StringView *sv);
+void sv_chop_end(StringView *sv);
 
-void sv_trim_back(StringView *sv);
+StringView sv_chop_until_delim(StringView *sv, char delim);
+StringView sv_chop_until_word(StringView *sv, StringView word);
+
+bool sv_starts_with(StringView sv, StringView prefix);
+bool sv_ends_with(StringView sv, StringView sufix);
+bool sv_equals(StringView sv, StringView other);
 
 #define SV_IMPLEMENTATION
 #ifdef SV_IMPLEMENTATION
 
-StringView sv_from_cstr(const char *cstr, size_t size)
+StringView sv_from_cstr(const char *cstr)
 {
-    StringView sv;
-    sv.data = cstr;
-    sv.size = size;
-    return sv;
+    return (StringView) {
+        .data = cstr,
+        .size = strlen(cstr),
+    };
 }
 
-// (Tilen 06.03.2026) WARNING: This assumes no repeating characters in word
-// (Tilen 06.03.2026) TODO: Use a suffix tree :) This would be cool
-bool cstr_ends_with(const char* cstr, const char* ends)
-{
-    char ch = ends[0];
-    size_t i = strlen(cstr)-1;
-    while (i > 0) {
-        if (cstr[i] == ch) {
-            break;
-        }
-        i--;
-    }
-    if (i == 0) return false;
-    assert(strlen(cstr)-i==strlen(ends)); 
-    for (size_t j=0;j<strlen(ends);++j) {
-        if (cstr[i+j] != ends[j]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool sv_equals(StringView sv1, StringView sv2)
-{
-    if (sv1.size != sv2.size) {
-        return false;
-    }
-    bool eq = true;
-    for (size_t i=0;i<sv1.size;++i) {
-        if (sv1.data[i] != sv2.data[i]) {
-            eq = false;
-            break;
-        }
-    }
-    return eq;
-}
-
-StringView sv_chop_until_delim(StringView sv, const char delim)
+// remove all spaces (` `) between the start of the string literal and first word
+void sv_trim_begin(StringView *sv)
 {
     size_t i = 0;
-    while (i < sv.size) {
-        if (sv.data[i] == delim) {
-            break;
-        }
-        i++;
+    while (i < sv->size && sv->data[i] == ' ') {
+        i += 1;
     }
-    StringView out = {
-        .size = i,
-        .data = sv.data
-    };
-    return out;
+    sv->size -= i;
+    sv->data += i;
 }
 
-// (Tilen 06.03.2026) WARNING: This assumes no repeating characters in word
-// (Tilen 06.03.2026) TODO: Use a suffix tree :) This would be cool
-bool sv_chop_until_word(StringView *sv, const char* ends)
+// remove all spaces (` `) between the last word of the string literal and end of string literal
+void sv_trim_end(StringView *sv)
 {
-    char ch = ends[0];
-    size_t i = sv->size-1;
-    while (i > 0) {
-        if (sv->data[i] == ch) {
-            break;
-        }
-        i--;
-    }
-    if (i == 0) return false;
-    assert(sv->size-i==strlen(ends)); 
-    for (size_t j=0;j<strlen(ends);++j) {
-        if (sv->data[i+j] != ends[j]) {
-            return false;
-        }
-    }
-    sv->size = i;
-    return true;
-}
-
-void sv_trim_back(StringView *sv)
-{
-    size_t i = sv->size-1;
+    size_t i = sv->size;
     while (i > 0 && sv->data[i] == ' ') {
         i -= 1;
     }
     sv->size = i;
 }
 
-#endif // SV_IMPLEMENTATION 
+// combine `sv_trim_left` and `sv_trim_right`
+void sv_trim(StringView *sv)
+{
+    sv_trim_begin(sv);
+    sv_trim_end(sv);
+}
+
+// Remove one character from the begining of the string literal
+void sv_chop_begin(StringView*sv)
+{
+    if (sv->size <= 0) {
+        return;
+    }
+    sv->data += 1;
+    sv->size -= 1;
+}
+
+// Remove one character from the end of the string literal
+void sv_chop_end(StringView *sv)
+{
+    if (sv->size <= 0) {
+        return;
+    }
+    sv->size -= 1;
+}
+
+StringView sv_chop_until_delim(StringView *sv, char delim)
+{
+    size_t i = 0;
+    while (i < sv->size && sv->data[i] != delim) {
+        i += 1;
+    }
+
+    StringView chopped = {
+        .data = sv->data,
+        .size = i,
+    };
+
+    sv->data += i;
+    sv->size -= i;
+
+    return chopped;
+}
+
+// TODO: (Tilen 23.03.2026) For practise we should implement a Trie here :)
+StringView sv_chop_until_word(StringView *sv, StringView word)
+{
+    size_t i = 0;
+    while (sv->size - i > word.size) {
+        if (sv->data[i] == word.data[0]) {
+            bool found_word = true;
+            for (size_t j=0;j<word.size;++j) {
+                if (sv->data[j+i] != word.data[j]) {
+                    found_word = false;
+                }
+            }
+            if (found_word) {
+                break;
+            }
+        }
+        i += 1;
+    }
+    StringView chopped = {
+        .data=sv->data,
+        .size=i,
+    };
+
+    sv->data += i;
+    sv->size -= i;
+
+    return chopped;
+}
+
+bool sv_starts_with(StringView sv, StringView prefix)
+{
+    size_t i = 0;
+    while (i < prefix.size) {
+        if (sv.data[i] == prefix.data[0]) {
+            if (i + prefix.size < sv.size) {
+                break;
+            }
+        }
+        i += 1;
+    }
+
+    bool found = true;
+    for (size_t j=0;j<prefix.size;++j) {
+        if (sv.data[i+j] != prefix.data[j]) {
+            found = false;
+        }
+    }
+
+    return found;
+}
+
+// Look from then end of the view for a sequence that matches the sufix view
+bool sv_ends_with(StringView sv, StringView sufix)
+{
+    size_t i = sv.size;
+    while (i >= sufix.size) {
+        if (sv.data[i] == sufix.data[0]) {
+            if (i + sufix.size == sv.size) {
+                break;
+            }
+        }
+        i -= 1;
+    }
+    if (i < sufix.size) {
+        return 0;
+    }
+    bool found = true;
+    for (size_t j=0;j<sufix.size;++j) {
+        if (sv.data[i+j] != sufix.data[j]) {
+            found = false;
+        }
+    }
+    return found;
+}
+
+bool sv_equals(StringView sv, StringView other)
+{
+    if (sv.size != other.size) return false;
+    bool eq = true;
+    for (size_t i=0;i<sv.size;++i) {
+        if (sv.data[i] != other.data[i]) {
+            eq = false;
+        }
+    }
+    return eq;
+}
+
+#endif // SV_IMPLEMENTATION
 #endif // SV_H_
